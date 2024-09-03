cbff87c7c1f9848553152b55029746f938a399ec
전우선 <150002756+wooxexn@users.noreply.github.com>
2024-09-03 17:34:11 +0900

    20240903

--- 1일차_0903.sql
+++ 1일차_0903.sql
@@ -1,4 +1,178 @@
---내림차순
+create table  acorntbl (
+    id  varchar2(10) primary key,
+    pw  varchar2(10) ,
+    name varchar2(10)
+);
+
+insert into acorntbl values('kdhyun', '4482', '김도현');
+insert into acorntbl values('kmg','1234', '김무궁');
+insert into acorntbl  values('ksjoong' ,'0000' , '김세중');
+insert into acorntbl  values('kimsh','6666','김세현');
+insert into acorntbl values('ksol', '1127', '김솔');
+insert into acorntbl values('kimjs', '0317', '김정석');
+insert into acorntbl values('kimcw','9876','김채운');
+insert into acorntbl values('backbw', '1111', '백바울'); 
+insert into acorntbl values('sbhak', '4482', '소병학');
+insert into acorntbl values('sbyoungH','0117','송병화'); 
+insert into acorntbl values('ssb','1126','송수빈');
+insert into acorntbl values('yjmin','0625','용지민');
+insert into acorntbl values('Leesmin','6789','이상민');
+insert into acorntbl  values('LeeHjin', '1234', '이효진');
+insert into acorntbl values('Jangkho','1234','장경호');
+insert into acorntbl values('Junws', '7777', '전우선');
+insert into acorntbl values('Junjh', '9999', '전지형');
+insert into acorntbl values('Jungbr', '2344', '정보란');
+insert into acorntbl values('jooSg', '1009', '주상길');
+insert into acorntbl values('jooHJ', '3456', '주형준');
+insert into acorntbl values('Choimgil', '9898', '최문길');
+insert into acorntbl values('ChoiHj', '5589', '최형진');
+insert into acorntbl values('HYJin', '1234', '하예진');
+insert into acorntbl values('Hjyoung', '9099', '황지영');
+commit;
+
+select * from acorntbl;
+
+--1일차
+--데이터 조회하기 SELECT
+
+-- * : 모든 컬럼 조회하기
+select * 
+from acorntbl;
+
+--원하는 컬럼 조회하기
+
+select id, name from acorntbl;
+ 
+-- 연결하기 
+select id || name from acorntbl;
+
+--컬럼 별칭(alias)주기
+--조회된 결과의 컬럼이 변경됨 (실제 데이터베이스 테이블의 컬럼이 변경되는 것은 아니다)
+select id|| name as name from acorntbl;
+
+select id as userid, name as username from acorntbl;
+
+--별칭에 공백이 있는경우 ""쌍따음표로 감싸야 한다
+select id as "user id" , name AS "user name"from acorntbl;
+
+--별칭줄 때 as 생략가능함
+select id userid, name username from acorntbl;
+
+--중복된 값을 제거하고 조회하기 distinct
+--emp 테이블 사용
+select * from emp;
+
+select job from emp;
+--12건 데이터 (행, 레코드) 중에서 중복된 것은 제외하고 조회할 수 있다.
+select distinct job from emp;
+
+--에이콘 학생명단 조회하기
+-- 모든행 (모든 레코드가 조회된다)
+select * from acorntbl;
+
+--원하는 조건만 조회하기
+-- where 절 사용하기
+
+--김씨 학생조회하기
+select *
+from acorntbl
+where name like '김%';
+
+--조건절에 올 수 있는 연산자
+select * from acorntbl where name = '김도현';
+
+--같지않다 !=
+select *
+from acorntbl
+where name != '김도현';
+
+-- > , >= , < , <=
+select * from emp;
+
+-- 테이블 명세 확인하기 (테이블스키마)
+desc emp;
+
+select ename, job , sal, comm
+from emp
+where sal >2500;
+
+select ename, job, sal ,comm
+from emp
+where sal <=2500;
+
+select ename, job, sal, comm
+from emp
+where sal <1000;
+
+-- between a and b (a에서 b까지, a가 작은 값 b가 큰값)
+select * 
+from emp
+where sal between 1000 and 2000;
+
+--조건이 여러개인 경우 AND ,OR
+--데이터베이스에서 문자를 표현할 때는 '     ' 사용
+--저장된 데이터내용을 비교할 때는 대소문자 구별함
+
+select *
+from emp
+where job = 'SALESMAN' and sal >= 1500;
+
+select *
+from emp
+where job = 'SALESMAN' or sal >=2000;
+
+-- in (a,b,c)
+select *
+from emp
+where job = 'CLERK' or job= 'MANAGER';
+
+select *
+from emp
+where job in('CLERK' , 'MANAGER');
+
+select *
+from emp
+where job not in('CLERK' , 'MANAGER');
+
+-- not 조건
+select *
+from emp
+where not sal >2000;
+
+--like 연산자
+select *
+from acorntbl
+where name like '김%';
+
+select *
+from acorntbl
+where name like '김_';
+
+--김으로 시작하고 뒤에 2자리 정해짐
+
+select *
+from acorntbl
+where name like '김__';
+
+--null  컬럼의 내용이 입력되지 않은 상태를 의미한 값( 정해지지 않은 것 , 미정의 값 ) , 연산시 주의해야 한다
+--null is null, is not null을 통해서 null인지 확인할 수 있다. ( = 연산자를 사용못함)
+select *
+from emp
+where comm is null ;
+
+--상여금에 100만큼 증가된 값으로 조회하기( 기존 데이터가 변경되는 것이 아니라 조회된 결과에만 반영)
+
+select ename, comm + 100 as comm
+from emp;
+
+--예상해 보기
+
+--null + 100 => null
+--null 값의 의미 : 값이 정해지지 않음을 의미 +100 => 값이 정해지지 않음
+
+--정렬하기
+-- 오름차순ㄴ , asc(생략가능)
+-- 내림차순 , desc(생략불가능)
 select *
 from emp
 order by sal desc;
@@ -10,14 +184,78 @@ order by sal;
 
 select *
 from emp
-order by jop;
+order by job;
 
 --여러개의 정렬기준을 줄 수 있다.
 select *
 from emp
-order by job, sal desc;
+order by job , sal desc;
 
-
---정렬시 컬럼의 순번을 사용할 수 있다.
+--정렬시 컬럼의 순번을 사용할 수 있다  (조회시 순번)
 select empno, ename, job
-from emp;
+from emp
+order by 2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 1일차_2_0903.sql
+++ 1일차_2_0903.sql
@@ -1,33 +1,96 @@
 --단일행함수
 
-
 --문자함수
-select * from professor;
 
---lower(), upper()  소문자로, 대문자로 변경해주는 함수
-
-select lower(name), upper(name)
+select *
 from professor;
 
--- substr()
--- 문자열의 일부를 추출
--- 시작인덱스 1부터
+--lower, upper  소문자로, 대문자로 변경해주는 함수
 
-
-select substr(name, 1, 3)
+select lower(name),upper(name)
 from professor;
 
+--substr()
+--문자열의 일부를 추출
+--시작인덱스 1부터
+select  substr(name,1,3)
+from professor;
 
-
--- instr() 함수
+--instr()함수
 --문자열에서 특정글자의 위치를 반환함
 
-
-select instr(name, 'A')
+select instr(name,'a')
 from professor;
 
-
-
---dual : 오라클에서 제공하는 임시테이블 , 함수를 테스트할 때 사용 할 수 있
-select instr('A-B-C-D' ,'-' )
+--dual : 오라클에서 제공하는 임시테이블 ,함수를 테스트 할 때 사용할 수 있음
+select instr('A-B-C-D','-' )
 from dual;
+
+select name , instr(tel ,')')
+from student;
+
+--LPAD()
+-- 왼쪽부터 특정문자로 채우는 기능
+select lpad(id,10, '-')
+from student;
+
+--RPAD()
+--오른쪽부터 특정문자로 채우는 기능
+
+select rpad(id,10,'*')
+from student;
+
+--replace() 기존의 문자를 새로운 문자로 대체함
+select replace(tel , '-','/') from student;
+
+select replace(name,'Ja','**') from student;
+
+select substr(name,1,2) from student;
+
+select replace(name,substr(name,1,2),'**') from student;
+
+--숫자함수
+
+--round()
+select round(8782.2358, 2)
+from dual;
+
+select trunc(8782.2358, 2)
+from dual;
+
+--날짜관련함수 
+--현재날짜가져오기
+select sysdate from dual;
+
+-- 두 날짜 사이의 개월 수 구하기
+select  months_between('05/09/03','04/12/31')
+from dual;
+
+--기타함수  NVL()   NVL2()
+select ename, comm+100
+from emp;
+
+--상여금 인상하는 조회
+--null인 경우는 0으로 처리하겠다
+select ename, nvl(comm,0)+100
+from emp;
+
+--nvl2()
+--null아니면
+--null이면
+select ename,comm, nvl2(comm,comm+500,0)
+from emp;
+
+--decode
+--01 vvip , 02 vip , 03 bronze
+select decode(m_grade,'01','vvip','02','vip','03','bronze') from member_tbl_11;
+
+--case when
+select m_name, m_point from member_tbl_11;
+
+select m_name, case when m_point>=3000 then '상'
+when m_point>=2000 then '중'
+else '하'
+end as result
+from member_tbl_11;
+  
